8f6ab322d81176213b64922e841dcf57567f29ac
Israfel Salazar <israfel.sr@gmail.com>
2025-03-31 16:44:14 +0200

    retrieval

--- evaluate.sh
+++ evaluate.sh
@@ -1,6 +1,6 @@
 export PYTHONPATH=$(pwd)
 
 python evaluation/evaluate.py \
---model_variant OpenCLIP \
---model_path /leonardo_work/EUHPC_D12_071/projects/complex-clip/models/negclip/negclip.pth \
+--model_variant HuggingFace \
+--model_path models/clip-vit-base-patch32 \
 --retrieval flickr
